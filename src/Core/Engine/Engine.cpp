fed2ec5fa4b250e4a4e8fbdf7020a10f46fd99a2
Pico <picoshot241@gmail.com>
2026-09-11 21:58:15 +0300

    core: add singleton engine class

---
+++ src/Core/Engine/Engine.cpp
@@ -0,0 +1,47 @@
+#include "Engine.hpp"
+#include "Core/Window/Window.hpp"
+
+namespace PicoEngine
+{
+
+Engine* Engine::s_instance = nullptr;
+
+Engine::Engine()
+{
+    PICO_ASSERT(s_instance == nullptr, "Only one Engine may exist");
+    s_instance = this;
+}
+
+Engine::~Engine()
+{
+    Shutdown();
+    s_instance = nullptr;
+}
+
+void Engine::Initialize()
+{
+    if (m_initialized)
+        return;
+
+    m_window      = std::make_unique<Window>(WindowDesc{});
+    m_initialized = true;
+    LOG_DEBUG("Engine initialized");
+}
+
+void Engine::Shutdown()
+{
+    if (!m_initialized)
+        return;
+
+    m_window.reset();
+    m_initialized = false;
+    LOG_DEBUG("Engine shut down");
+}
+
+Window& Engine::GetWindow()
+{
+    PICO_ASSERT(m_window == nullptr, "Engine::GetWindow called before Initialize()");
+    return *m_window;
+}
+
+} // namespace PicoEngine
---
+++ src/Core/Engine/Engine.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Core/Debug/Debug.hpp"
+
+namespace PicoEngine
+{
+
+class Window;
+
+class Engine
+{
+  public:
+    Engine();
+    ~Engine();
+
+    Engine(const Engine&)            = delete;
+    Engine& operator=(const Engine&) = delete;
+    Engine(Engine&&)                 = delete;
+    Engine& operator=(Engine&&)      = delete;
+
+    static Engine& Get()
+    {
+        PICO_ASSERT(s_instance != nullptr, "Engine accessed before creation");
+        return *s_instance;
+    }
+
+    void Initialize();
+    void Shutdown();
+    bool IsInitialized() const
+    {
+        return m_initialized;
+    }
+
+    Window& GetWindow();
+
+  private:
+    static Engine* s_instance;
+
+    bool m_initialized = false;
+
+    std::unique_ptr<Window> m_window;
+};
+
+} // namespace PicoEngine
