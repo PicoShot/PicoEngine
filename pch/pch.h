62fe5f72ecabdb274bc5cf12bd17876d44920dba
Pico <picoshot241@gmail.com>
2026-09-12 10:17:01 +0300

    library: include glm

--- pch/pch.h
+++ pch/pch.h
@@ -25,3 +25,16 @@
 
 // SDL3
 #include <SDL3/SDL.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/simd/common.h>
+#include <glm/simd/exponential.h>
+#include <glm/simd/geometric.h>
+#include <glm/simd/integer.h>
+#include <glm/simd/matrix.h>
+#include <glm/simd/packing.h>
+#include <glm/simd/trigonometric.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
